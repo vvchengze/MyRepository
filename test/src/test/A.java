15111040dd56c28b76b2d2b2350b4cf0dc372cf8
chengze <291073167@qq.com>
2020-04-23 01:19:29 +0800

    del

--- test/src/test/A.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class A {
-
-	void a(){
-        System.out.println("parent'a()");
-    }
-
-}
--- test/src/test/IOException.java
+++
@@ -1,5 +0,0 @@
-package test;
-
-public class IOException extends Exception {
-
-}
